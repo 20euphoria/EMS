61fa26043484102d576763437c4e22f22310e7e5
Suraj Mahamuni <surajsm@maveric-systems.com>
2022-04-05 21:19:00 +0530

    initialise DB

--- Department/models.py
+++ Department/models.py
@@ -1,3 +1,10 @@
+from operator import mod
+from statistics import mode
 from django.db import models
+from User.models import Employee
 
 # Create your models here.
+class Department(models.Model):
+    deptid = models.AutoField(primary_key=True, unique=True)
+    eid = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    deptname = models.CharField(max_length=80)
--- User/models.py
+++ User/models.py
@@ -1,3 +1,10 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    eid = models.AutoField(primary_key=True, unique=True)
+    ename = models.CharField(max_length=80)
+    ecity = models.CharField(max_length=80)
+    etype = models.CharField(max_length=80)
+    edomain = models.CharField(max_length=80)
